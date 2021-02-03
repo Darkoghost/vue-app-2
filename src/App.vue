b95be4b2457491ecbae40d5c3d887750208a5b03
Darkoghost <jujuiolm@hotmail.fr>
2021-02-03 10:33:40 +0100

    petit changement et connexion api

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,40 @@
 <template>
   <div id="app">
     <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+      <router-link to="/">Accueil</router-link> |
+      <router-link to="/perso">Personnages</router-link> |
+      <router-link to="/world">Mondes</router-link> |
+      <router-link to="/about">A propos</router-link>
     </div>
     <router-view/>
   </div>
 </template>
+
+<script>
+import axios from "axios";
+
+export default {
+  name: 'app',
+  data() {
+    return {
+      test: []
+    }
+  },
+  beforeCreate() {
+    axios
+        .get('https://swapi.dev/api/people/1/')
+    .then(res => {
+      this.test = res.data
+      console.log(res.data)
+    })
+
+  }
+}
+
+
+</script>
+
+
 
 <style>
 #app {
@@ -14,7 +42,8 @@
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
+  color: black;
+  background-image: url("images/background2.jpg");
 }
 
 #nav {
