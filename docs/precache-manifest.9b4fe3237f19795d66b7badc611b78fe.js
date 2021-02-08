24efad67f412914fec119cf90bd377b4a2b0cf8f
Darkoghost <jujuiolm@hotmail.fr>
2021-02-08 13:48:57 +0100

    ajout table perso,monde et pagination

---
+++ docs/precache-manifest.9b4fe3237f19795d66b7badc611b78fe.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1a27d94b427d5e0d2adc",
+    "url": "/vue-app-2/css/app.b0c35368.css"
+  },
+  {
+    "revision": "88dc84905722abe39acc",
+    "url": "/vue-app-2/css/chunk-vendors.64d43e69.css"
+  },
+  {
+    "revision": "bdb932627205c4f6015ae0ee42f213c2",
+    "url": "/vue-app-2/img/accueil.bdb93262.jpg"
+  },
+  {
+    "revision": "1a567f37f23dbace9e5780446c8a05c0",
+    "url": "/vue-app-2/img/background2.1a567f37.jpg"
+  },
+  {
+    "revision": "ee2bbb89d11efb6cc03b837f286e89c7",
+    "url": "/vue-app-2/index.html"
+  },
+  {
+    "revision": "f38660cb90dcbccf8683",
+    "url": "/vue-app-2/js/about.fe34cce6.js"
+  },
+  {
+    "revision": "1a27d94b427d5e0d2adc",
+    "url": "/vue-app-2/js/app.2922b0f8.js"
+  },
+  {
+    "revision": "88dc84905722abe39acc",
+    "url": "/vue-app-2/js/chunk-vendors.3e599af1.js"
+  },
+  {
+    "revision": "a20c75afa1f0a984f0883b71519b2755",
+    "url": "/vue-app-2/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/vue-app-2/robots.txt"
+  }
+]);
